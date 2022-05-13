47f77af6a175e987e6b5507b6121362d8ad4c28d
b34rd <81767471+b34rd-tek@users.noreply.github.com>
2022-05-13 14:26:50 -0500

    Update report_issues.py

--- trivy_report/report_issues.py
+++ trivy_report/report_issues.py
@@ -32,7 +32,6 @@ def main():
     data: ReportDict = json.load(open(filename, "rb"))
     if not isinstance(data, dict):
         abort(f"Data in json file {filename} does not contain a dictionary")
-    github_summary = os.environ.get("GITHUB_STEP_SUMMARY")
     github_event = os.environ.get("GITHUB_EVENT_NAME")
     github_repo = os.environ.get("GITHUB_REPOSITORY")
     github_token = os.environ.get("GITHUB_TOKEN")
@@ -114,7 +113,6 @@ def main():
                 proc.communicate()
                 if proc.returncode != 0:
                     abort("Failed to create comment with `gh` cli")
-        github_summary = github_summary + "## Trivy Scan Summary: <br> " + comment_content
     else:
         # Generate issues
         for issue in issues:
@@ -143,7 +141,6 @@ def main():
             proc.communicate()
             if proc.returncode != 0:
                 abort("Failed to create issue with `gh` cli")
-        github_summary = github_summary + "## Trivy Scan Summary: <br> " + comment_content
         else:
             print("No new vulnerabilities found")
 
