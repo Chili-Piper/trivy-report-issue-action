0b8253b0f48e3ea2ed8c76ec011a43062f4a3cb0
b34rd <81767471+b34rd-tek@users.noreply.github.com>
2022-05-13 13:16:16 -0500

    Update report_issues.py

--- trivy_report/report_issues.py
+++ trivy_report/report_issues.py
@@ -100,7 +100,7 @@ def main():
                     "comment",
                     pr_number,
                     "--body",
-                    "Test Comment",
+                    comment_content,
                 ]
                 + extra_args
             )
